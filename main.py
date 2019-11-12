c8edb2d6365b69981936efd4bfa79c42dcac8b9d
Artur Morelle Nista <arturnista@gmail.com>
2019-11-12 00:52:00 -0300

    Added loop cache. Improved code struct

--- main.py
+++ main.py
@@ -1,81 +1,140 @@
 import sys
 
-# Verify if a filename was entered
-if(len(sys.argv) < 2):
-    print("Please, enter the file name!")
-    exit(1)
+MIN_VALUE = 0
+MAX_VALUE = 255
 
-# Open the .bf file
-filename = sys.argv[1]
-if(".bf" not in filename):
-    filename += ".bf"
+# Read filename from args
+def getFilenameEntered():
+    # Verify if a filename was entered
+    if(len(sys.argv) < 2):
+        print("Please, enter the file name!")
+        exit(1)
 
-f = open(filename, "r")
-code = f.read()
+    # Open the .bf file
+    filename = sys.argv[1]
+    if(".bf" not in filename):
+        filename += ".bf"
 
-# Prepare split code data
-codeData = list(code)
-codeLength = len(codeData)
-codeIndex = 0
+    return filename
 
-# Data
-arrayData = [ 0 ]
-ptr = 0
+# Read code file
+def readCode(filename):
+    f = open(filename, "r")
+    code = f.read()
 
-# Loop queue
-loop = []
+    # Split the commands
+    codeData = list(code)
+    codeIndex = 0
 
-# Process the code
-while codeIndex < codeLength:
-    currentCommand = codeData[codeIndex]
-    if(currentCommand == '.'): # Print
-        print(chr(arrayData[ptr]), end = '')
-    elif(currentCommand == ','): # Accept (nop)
-        accepted = input()
-        if(len(accepted) > 0):
-            arrayData[ptr] = ord(accepted)
+    # Cleans code, removing comments
+    availableCommands = ['.', ',', '+', '-', '>', '<', '[', ']']
+    while codeIndex < len(codeData):
+        if(codeData[codeIndex] not in availableCommands):
+            del codeData[codeIndex]
         else:
-            arrayData[ptr] = 10
-    elif(currentCommand == '+'): # Increment
-        arrayData[ptr] += 1
-        if(arrayData[ptr] > 255):
-            arrayData[ptr] = 0
-    elif(currentCommand == '-'): # Decrement
-        arrayData[ptr] -= 1
-        if(arrayData[ptr] < 0):
-            arrayData[ptr] = 255
-    elif(currentCommand == '>'): # Next memory
-        ptr += 1
-        # If new memory index is accessed, create the new memory
-        if(len(arrayData) >= ptr):
-            arrayData.append(0)
-    elif(currentCommand == '<'): # Previous memory
-        ptr -= 1
-        # If access a new memory before last, create a new memory in the beginning
-        if(ptr < 0):
-            ptr = 0
-            arrayData.insert(0, 0)
-    elif(currentCommand == '['):
-        if(arrayData[ptr] != 0):
-            loop.append(codeIndex)
-        else:
-            # Find end of the loop
-            loopLookAhead = []
             codeIndex += 1
-            while True:
-                if(codeData[codeIndex] == '['):
-                    loopLookAhead.append(codeIndex)
-                elif(codeData[codeIndex] == ']'):
-                    if(len(loopLookAhead) == 0):
-                        break
-                    else:
-                        loopLookAhead.pop()
-                codeIndex += 1
-    elif(currentCommand == ']'):
-        loopCodeIndex = loop.pop()
-        if(arrayData[ptr] != 0):
-            codeIndex = loopCodeIndex - 1
-    else:  # Comment
-        pass
 
-    codeIndex += 1
+    return codeData
+
+# Cache start and ending loop positions
+def cacheLoopData(codeData):
+    loop = []
+    cache = {}
+
+    codeLength = len(codeData)
+
+    for i in range(codeLength):
+        currentCommand = codeData[i]
+        if(currentCommand == '['):
+            loop.append(i)
+        elif(currentCommand == ']'):
+            if(len(loop) == 0):
+                print("Syntax error!\n']' at position " +
+                    str(i) + " has no matching '['.")
+                exit(1)
+
+            index = loop.pop()
+            cache[index] = i
+
+    if(len(loop) > 0):
+        print("Syntax error!\n'[' at position " +
+            str(loop[0]) + " has no matching ']'.")
+        exit(1)
+
+    return cache
+
+def main():
+    # Get filaname
+    filename = getFilenameEntered()
+
+    # Get the code data
+    codeData = readCode(filename)
+    codeLength = len(codeData)
+    codeIndex = 0
+
+    # "Memeory" Data
+    arrayData = [0]
+    ptr = 0
+
+    # Loop queue
+    loop = []
+    loopCache = cacheLoopData(codeData)
+
+    # Process the code
+    while codeIndex < codeLength:
+
+        # Get current command
+        currentCommand = codeData[codeIndex]
+
+        if(currentCommand == '+'):  # Increment
+            arrayData[ptr] += 1
+            if(arrayData[ptr] > MAX_VALUE):
+                arrayData[ptr] = MIN_VALUE
+
+        elif(currentCommand == '-'):  # Decrement
+            arrayData[ptr] -= 1
+            if(arrayData[ptr] < MIN_VALUE):
+                arrayData[ptr] = MAX_VALUE
+
+        elif(currentCommand == '>'):  # Next memory
+            ptr += 1
+            # If new memory index is accessed, create the new memory position
+            if(len(arrayData) >= ptr):
+                arrayData.append(0)
+
+        elif(currentCommand == '<'):  # Previous memory
+            ptr -= 1
+            # If access a new memory before last, create a new memory in the beginning
+            if(ptr < 0):
+                ptr = 0
+                arrayData.insert(0, 0)
+
+        elif(currentCommand == '['): # Start loop
+            if(arrayData[ptr] != 0):
+                loop.append(codeIndex)
+            else:
+                codeIndex = loopCache[codeIndex]
+
+        elif(currentCommand == ']'): # End loop
+            loopCodeIndex = loop.pop()
+            if(arrayData[ptr] != 0):
+                codeIndex = loopCodeIndex - 1
+
+        elif(currentCommand == '.'):  # Print
+            print(chr(arrayData[ptr]), end='')
+
+        elif(currentCommand == ','):  # Accept
+            accepted = input()
+            if(len(accepted) > 0):
+                arrayData[ptr] = ord(accepted)
+            else:
+                arrayData[ptr] = 10
+
+        else:  # Comment
+            pass
+
+        codeIndex += 1
+
+
+if(__name__ == "__main__"):
+    main()
